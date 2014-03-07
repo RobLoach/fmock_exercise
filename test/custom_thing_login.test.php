26db8e554b2d336fb280bdd85a974088d269445e
Rob Loach <robloach@gmail.com>
2014-03-07 12:57:23 -0500

    Add my tests

--- test/custom_thing_login.test.php
+++ test/custom_thing_login.test.php
@@ -1,18 +1,23 @@
 <?php
 
-require_once '../vendor//function_mock.php';
+require_once '../vendor/function_mock.php';
+require_once '../custom_thing_login.module';
 
-class CustomThingTest extends PHPUnit_Framework_TestCase
-{
-  public function customThingLoginPermissionTest() {
-    
+class CustomThingTest extends PHPUnit_Framework_TestCase{
+
+  public function testThingLoginPermissionTest() {
+    FunctionMock::createMockFunctionDefinition('t');
+
+    FunctionMock::stub('t', 'login without remote_api', array('login without remote_api'));
+    FunctionMock::stub('t', 'Login to the site without remote_api authentication.', array('Login to the site without remote_api authentication.'));
+
+    $result = custom_thing_login_permission();
+    $this->assertEquals($result['login without remote_api']['title'], 'login without remote_api');
+    $this->assertEquals($result['login without remote_api']['description'], 'Login to the site without remote_api authentication.');
   }
   
   
-  public function customThingLoginInitTest() {
+  public function testThingLoginInitTest() {
     
   }  
 }
-
-
-?>
